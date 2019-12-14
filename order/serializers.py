eba4fbc38b1b1d94c67c4227b38d903fc50a3989
XXXDENDACION <icesfrost@gmail.com>
2019-12-15 02:54:28 +0300

    Исправил поля списка заказов

--- order/serializers.py
+++ order/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-from order.models import order, deliveryAdress
+from order.models import order, deliveryAdress, statusDelivery
 from clients.models import Client
+from django.contrib.auth.models import User
 
 class ClientFioSerializer(serializers.ModelSerializer):
     class Meta:
@@ -8,15 +9,28 @@ class ClientFioSerializer(serializers.ModelSerializer):
         fields = ['fio']
 
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username']
+
+
 class DeliveryAdressSerializer(serializers.ModelSerializer):
     class Meta:
         model = deliveryAdress
-        fields = ['client']
+        fields = ['adress']
+
+class StatusDeliverySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = statusDelivery
+        fields = ['name']
 
 
 class OrdersListSerializer(serializers.ModelSerializer):
     client = ClientFioSerializer(required=True)
     deliveryAdress = DeliveryAdressSerializer(required=True)
+    operator = UserSerializer(required=True)
+    statusDelivery = StatusDeliverySerializer(required=True)
     class Meta:
         model = order
         fields = ['client', 'operator', 'createOrder', 'statusDelivery', 'deliveryAdress']
