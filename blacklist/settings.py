4524ffd3c74cc0d93ed017385af3d2408cead518
Kirill Pakhtusov <pakhtusovkirill@gmail.com>
2020-01-07 22:28:26 +0300

    fix setting

--- blacklist/settings.py
+++ blacklist/settings.py
@@ -147,6 +147,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
