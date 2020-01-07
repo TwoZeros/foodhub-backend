084e3a4f10faa53dd1a0fde48a187deccbd71ea0
Kirill Pakhtusov <pakhtusovkirill@gmail.com>
2020-01-08 01:03:49 +0300

    fix

--- blacklist/settings.py
+++ blacklist/settings.py
@@ -162,7 +162,7 @@ STATIC_URL = '/static/' # URL для шаблонов
  
  
 STATICFILES_DIRS = [
-    'blacklist/static',
+    '/static',
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 db_from_env = dj_database_url.config(conn_max_age=500)
